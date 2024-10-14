2e8dbc5421a58b7545ac4b2701076b392aaf1634
Ricardo Rufino <cadinhorufjr@icloud.com>
2024-10-14 17:26:36 -0300

    chore: add initial tables

--- app/_components/sidebart.tsx
+++ app/_components/sidebart.tsx
@@ -5,13 +5,12 @@ import SidebarButton from './sidebar-button'
 const Sidebar = () => {
   return (
     <div className='flex h-screen w-64 flex-col bg-white'>
-      <div className='p-4'>
-        <Image width={130} height={130} src='/logo-toca.avif' alt='Logo Toca do Coelho' />
+      <div className='flex items-center justify-center p-4'>
+        <Image width={100} height={100} src='/logo.png' alt='ABDUZ' priority className='h-auto w-auto' />
       </div>
       <div className='flex flex-col gap-2 p-2'>
         <SidebarButton href='/'>
-          <LayoutGrid size={20} />
-          Dashboard
+          <LayoutGrid size={20} /> Dashboard
         </SidebarButton>
         <SidebarButton href='/products'>
           <Package size={20} />
