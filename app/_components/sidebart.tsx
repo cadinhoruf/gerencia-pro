b0ef8fd39fa645004a264b0533430761c632707d
Ricardo Rufino <cadinhorufjr@icloud.com>
2024-10-14 14:01:50 -0300

    feat: sidebar and create ohter pages

--- app/_components/sidebart.tsx
+++ app/_components/sidebart.tsx
@@ -1,15 +1,27 @@
 import Image from 'next/image'
+import { LayoutGrid, Package, ShoppingBasket } from 'lucide-react'
+import SidebarButton from './sidebar-button'
 
 const Sidebar = () => {
   return (
-    <div className='flex flex-col items-center bg-white w-64 h-screen'>
+    <div className='flex h-screen w-64 flex-col bg-white'>
       <div className='p-4'>
-        <Image width={50} height={50} src='/logo.avif' alt='Logo Toca do Coelho' />
+        <Image width={130} height={130} src='/logo-toca.avif' alt='Logo Toca do Coelho' />
       </div>
-      <div className='flex flex-col gap-2 p-2'></div>
-      <button className='px-6 py-3'>Dashboard</button>
-      <button className='px-6 py-3'>Produtos</button>
-      <button className='px-6 py-3'>Vendas</button>
+      <div className='flex flex-col gap-2 p-2'>
+        <SidebarButton href='/'>
+          <LayoutGrid size={20} />
+          Dashboard
+        </SidebarButton>
+        <SidebarButton href='/products'>
+          <Package size={20} />
+          Produtos
+        </SidebarButton>
+        <SidebarButton href='/sales'>
+          <ShoppingBasket size={20} />
+          Vendas
+        </SidebarButton>
+      </div>
     </div>
   )
 }
